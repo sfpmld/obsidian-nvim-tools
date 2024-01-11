5c2dd64a64c8a4a4a30accedac55e479298a3f5e
xavier <xavier.abenaqui@gmail.com>
2024-01-12 00:36:17 +0100

    Fix(create usercmd): fix bad create user initialization

--- plugin/obsidian-nvim-tools.lua
+++ plugin/obsidian-nvim-tools.lua
@@ -1,18 +1,18 @@
 local vim = vim
-local nvim_create_user_command = vim.api.nvim_set_hl
+local nvim_set_hl, nvim_create_user_command = vim.api.nvim_set_hl, vim.api.nvim_create_user_command
 
-nvim_create_user_command("ObsidianToolsConvertH1ToWikiLinks", function()
-	require("obsidian-nvim-tools").convertH1ToWikiLinks()
+nvim_create_user_command('ObsidianToolsConvertH1ToWikiLinks', function()
+  require('obsidian-nvim-tools').convertH1ToWikiLinks()
 end, {})
 
-nvim_create_user_command("ObsidianToolsConvertH2ToWikiLinks", function()
-	require("obsidian-nvim-tools").convertH2ToWikiLinks()
+nvim_create_user_command('ObsidianToolsConvertH2ToWikiLinks', function()
+  require('obsidian-nvim-tools').convertH2ToWikiLinks()
 end, {})
 
-nvim_create_user_command("ObsidianToolsConvertH3ToWikiLinks", function()
-	require("obsidian-nvim-tools").convertH3ToWikiLinks()
+nvim_create_user_command('ObsidianToolsConvertH3ToWikiLinks', function()
+  require('obsidian-nvim-tools').convertH3ToWikiLinks()
 end, {})
 
-nvim_create_user_command("ObsidianToolsConvertH4ToWikiLinks", function()
-	require("obsidian-nvim-tools").convertH4ToWikiLinks()
+nvim_create_user_command('ObsidianToolsConvertH4ToWikiLinks', function()
+  require('obsidian-nvim-tools').convertH4ToWikiLinks()
 end, {})
